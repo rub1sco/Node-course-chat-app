0e92712c6fb3de013f1412d9ddee84d822d8fdd8
Tom <tom.lucero@me.com>
2018-08-08 07:43:02 -0600

    security fixes with updates

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '/../public');
 const express = require('express');
-const app = express();
 const port = process.env.PORT || 3000;
+const http = require('http');
+const socketIO= require('socket.io');
+
+var app = express();
+var server = http.createServer(app);
+var io= socketIO(server);
+
 
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+server.listen(port,()=>{
   console.log(`server is up on ${port}`)
 })
